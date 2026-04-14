bf4f70707e8329ea72a2ac392316df7c135a4148
Sammy Hill <myallio94@gmail.com>
2026-04-14 22:35:22 +1000

    route failed messages to DLQ topic

--- consumer/src/index.ts
+++ consumer/src/index.ts
@@ -10,9 +10,12 @@ const kafka = new Kafka({
 
 const consumer = kafka.consumer({ groupId: 'notification-group' })
 const registry = new SchemaRegistry({ host: process.env.SCHEMA_REGISTRY_URL ?? 'http://localhost:8081' })
+const producer = kafka.producer()
 
 async function runConsumer(): Promise<void> {
     await consumer.connect()
+    // producer for dlq since we create topics in dockercompose
+    await producer.connect()
     console.log('Consumer connected')
 
     await consumer.subscribe({
@@ -23,19 +26,23 @@ async function runConsumer(): Promise<void> {
     await consumer.run({
         autoCommit: false,
         eachMessage: async ({ topic, partition, message }) => {
-            // decodes schema msg to json obj using schemaid
-            if (!message.value) return // can be null but registry.decode expects buffer
-            const decodedValue = await registry.decode(message.value)
-            // write to db
-            await insertEvent(decodedValue)
-            // sets offset manually
-            await consumer.commitOffsets([{ topic: topic, partition: partition, offset: String(Number(message.offset) + 1) }])
-            console.log({ decodedValue })
-            console.log('Received message:')
-            console.log('  topic:', topic)
-            console.log('  partition:', partition)
-            console.log('  offset:', message.offset)
-        },
+            try {
+                // decodes schema msg to json obj using schemaid
+                if (!message.value) return // can be null but registry.decode expects buffer
+                const decodedValue = await registry.decode(message.value)
+                // write to db
+                await insertEvent(decodedValue)
+                // sets offset manually
+                await consumer.commitOffsets([{ topic: topic, partition: partition, offset: String(Number(message.offset) + 1) }])
+                console.log({ decodedValue })
+                console.log('Received message:')
+                console.log('  topic:', topic)
+                console.log('  partition:', partition)
+                console.log('  offset:', message.offset)
+            } catch (error) {
+                await producer.send({ topic: 'dlq', messages: [{ value: message.value }] })
+            }
+        }
     })
 }
 
