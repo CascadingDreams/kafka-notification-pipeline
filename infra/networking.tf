86b469cf8c660040e097a2bc018c008a17020a7e
Sammy <myallio94@gmail.com>
2026-04-28 15:59:50 +1000

    refactor: split compute and networking resources into separate tf files

--- infra/networking.tf
+++ infra/networking.tf
@@ -41,19 +41,3 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
-
-resource "aws_key_pair" "ec2_key" {
-  key_name = "${var.project_name}-kafka-key"
-  public_key = file("~/.ssh/kafka-ec2.pub")
-}
-
-resource "aws_instance" "kafka" {
-  ami           = "ami-0310483fb2b488153"
-  instance_type = var.kafka_instance
-  vpc_security_group_ids = [aws_security_group.allow_ec2.id]
-  key_name = aws_key_pair.ec2_key.key_name
-
-  tags = {
-    Name = "${var.project_name}-kafka"
-  }
-}
